240425708c09c679481f4d714ddc8a7120f3afa6
Arvind <singharvi69@gmail.com>
2021-07-08 18:09:14 +0530

    added

--- api/Models/User.js
+++ api/Models/User.js
@@ -4,15 +4,15 @@ const UserSchema = new mongoose.Schema({
     username:{
         type: String,
         required: true,
-        unique: true
+        unique: true,
     }, 
     email:{
         type: String,
         required: true,
-        unique: true
+        unique: true,
     }, 
     password: {
-        type: string,
+        type: String,
         required: true,
     },
      profilePic: {
--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-
 const port = 5000;
 const dotenv = require("dotenv");
+const authRoute = require("./Routes/Auth");
 dotenv.config();
+app.use(express.json());
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL,
     {
@@ -12,9 +13,11 @@ mongoose.connect(process.env.MONGO_URL,
         useCreateIndex: true,
     }).then(console.log("Connected")).catch((err)=> {console.log(err)});
 
-app.use("/", (req, res) => {
-    console.log("Hey This is Arvind singh")
-})
+// app.use("/", (req, res) => {
+//     console.log("Hey This is Arvind singh")
+app.use("/api/auth", authRoute);
+
 app.listen(port, () => {
     console.log("Server is running at port", port);
-})
+});
+
